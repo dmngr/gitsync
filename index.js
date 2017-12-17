9e6a9253da97bdcf58c7830f950b17d78f8caac6
ioanniswd <ioanniswd@gmail.com>
2017-12-17 22:03:34 +0200

    Removed unnecessary nodegit module
     On branch master
    
     Changes to be committed:
    	modified:   index.js
    	modified:   package-lock.json
    	modified:   package.json

--- index.js
+++ index.js
@@ -8,6 +8,5 @@ const exec = Promise.promisify(require('child_process').exec);
 const find = Promise.promisifyAll(require('find'));
 const homedir = require('os').homedir();
 const colors = require('colors/safe');
-const Git = require("nodegit");
 
 // exec('find . -type d -regextype sed -regex ".*/^(.git)^(node_modules)/g"');
